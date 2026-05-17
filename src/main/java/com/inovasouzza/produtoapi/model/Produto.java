e33604dae0d2b92a5949d13c23e373beef56b762
MarcosSouzaa <marcossouza.git@gmail.com>
2026-05-17 18:50:31 -0300

    mapeamento na classe Produto para o DB

--- src/main/java/com/inovasouzza/produtoapi/model/Produto.java
+++ src/main/java/com/inovasouzza/produtoapi/model/Produto.java
@@ -1,5 +1,9 @@
 package com.inovasouzza.produtoapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,11 +15,18 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @ToString /*Criado esse toString para representar meu produto no controller*/
+@Entity
+@Table(name = "produto") /*Se o nome da tabela for o mesmo no DB, não precisa colocar o nome*/
 public class Produto {
-
+    /*Se o nome for o mesmo das colunas no Banco, não precisa colocar o @Column*/
+    @Id
+    @Column(name = "id")
     private String id;
+    @Column(name = "nome")
     private String nome;
+    @Column(name = "descricao")
     private String descricao;
+    @Column(name = "preco")
     private BigDecimal preco;
 }
 
